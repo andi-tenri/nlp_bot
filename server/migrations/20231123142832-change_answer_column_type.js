44180c22b6a1fe528c731484322b461cf7dd5ba3
Muhammad Qadri <mqad21@gmail.com>
2023-11-23 21:37:25 +0700

    update migration

--- server/migrations/20231123142832-change_answer_column_type.js
+++ server/migrations/20231123142832-change_answer_column_type.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('answers', 'answer', {
+    await queryInterface.changeColumn('Datasets', 'answer', {
       type: Sequelize.TEXT
     })
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('answers', 'answer', {
+    await queryInterface.changeColumn('Datasets', 'answer', {
       type: Sequelize.STRING
     })
   }
